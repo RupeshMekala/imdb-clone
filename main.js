2d899ddddfd53f82413bb4f50b5d561301186a2e
Rupesh <rupesh21092004@gmail.com>
2025-06-15 18:57:49 +0530

    added favourites.html, made index.html responsive

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-const element = <h1>Hello IMDb</h1>
+
+
+const favBtn= document.getElementById('fav')
+if(favBtn){
+    favBtn.addEventListener('click',function(){
+    window.location.href = 'favourites.html';
+  })
+}
+
+
+const imdbIcon = document.getElementById('logo')
+if(imdbIcon){
+    imdbIcon.addEventListener('click', function(){
+    window.location.href = 'index.html';
+  })
+}
+
+
 
