b9cfcf7dee3a665cd3016c533e9e77d1ecf9c861
Rupesh <rupesh21092004@gmail.com>
2025-06-20 07:26:43 +0530

    written main.js

--- main.js
+++ main.js
@@ -1,19 +1,80 @@
-
-
-const favBtn= document.getElementById('fav')
-if(favBtn){
-    favBtn.addEventListener('click',function(){
-    window.location.href = 'favourites.html';
-  })
-}
-
-
 const imdbIcon = document.getElementById('logo')
 if(imdbIcon){
     imdbIcon.addEventListener('click', function(){
     window.location.href = 'index.html';
-  })
+  }) 
 }
+
+const apiKey = '4574d603';
+const searchInput = document.getElementById('movie-searchbox');
+const suggestionList = document.getElementById("search-list");
+
+searchInput.addEventListener('input', function () {
+  const searchTerm = this.value.trim();
+
+  if (searchTerm === '') {
+    suggestionList.innerHTML = '';
+    return;
+  }
+
+  fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${encodeURIComponent(searchTerm)}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.Response === 'True') {
+        Suggestionsdisplay(data.Search);
+      } else {
+        suggestionList.textContent = "No result found";
+      }
+    })
+    .catch(error => console.error('Error fetching search results:', error));
+});
+
+function Suggestionsdisplay(suggestions) {
+  const maxSuggestions = 5;
+
+  if (suggestions.length === 0) {
+    suggestionList.textContent = "No result found";
+    return;
+  }
+
+  suggestionList.innerHTML = '';
+
+  for (let i = 0; i < Math.min(suggestions.length, maxSuggestions); i++) {
+    const suggestion = suggestions[i];
+    const suggestionLink = document.createElement('div');
+    suggestionLink.classList.add('suggestionLink', 'suggestionLinkDisplay');
+
+    const posterImg = document.createElement('img');
+    posterImg.src = suggestion.Poster !== 'N/A' ? suggestion.Poster : 'src/samplePoster.jpg';
+    posterImg.alt = suggestion.Title;
+    suggestionLink.appendChild(posterImg);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('info');
+
+    const titleSpan = document.createElement('div');
+    titleSpan.textContent = suggestion.Title;
+    titleSpan.style.paddingTop = '2vh';
+    infoDiv.appendChild(titleSpan);
+
+    const yearOfRelease = document.createElement('div');
+    yearOfRelease.textContent = "Year of release: " + suggestion.Year;
+    infoDiv.appendChild(yearOfRelease);
+
+    suggestionLink.appendChild(infoDiv);
+
+    suggestionLink.addEventListener('click', function () {
+      suggestionList.innerHTML = '';
+      localStorage.setItem('movieInfo', JSON.stringify(suggestion));
+      window.location.href = 'movieInformation.html';
+    });
+
+    suggestionList.appendChild(suggestionLink);
+  }
+}
+
+
+
 
 
 
